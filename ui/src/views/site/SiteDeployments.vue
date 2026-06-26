07a3607acd7e99657821243f01f3f453466bbbbf
Fabiotech1 <faboy46@gmail.com>
2026-06-26 12:43:24 +0100

    fix: add undo icon to rollback button

--- ui/src/views/site/SiteDeployments.vue
+++ ui/src/views/site/SiteDeployments.vue
@@ -103,6 +103,7 @@
                      :disabled="isDeploying || rollingBack"
                      :loading="rollingBack"
                      @click="handleRollback">
+            <svg class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 10h10a5 5 0 015 5v2M3 10l4-4m-4 4l4 4" /></svg>
             Rollback
           </AppButton>
           <span v-else></span>
